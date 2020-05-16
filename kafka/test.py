8c91121d47bc4396d4a9264348792085e5318b82
Your Name <you@example.com>
2020-05-16 10:39:22 +0800

    kafka

---
+++ kafka/test.py
@@ -0,0 +1,15 @@
+from kafka import KafkaProducer
+import json
+
+def producerPay(account, salaNum):
+    producer = KafkaProducer(bootstrap_servers=['172.20.10.8:9092'])
+    data = {
+        "name": account,
+        "age": salaNum
+    }
+    data = json.dumps(data).encode("utf-8")
+    future = producer.send("pay", data)
+    producer.flush()
+    return producer
+if __name__ == '__main__':
+    producerPay("1614001432", 300)
